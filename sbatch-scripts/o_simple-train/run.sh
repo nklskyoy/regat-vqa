36dc5494362966fb627232669576799cfb9ffdd4
Oleksandr Nikolskyy <onikolskyy@gmail.com>
2023-05-24 09:11:09 +0200

    fix o_simple-train

--- sbatch-scripts/o_simple-train/run.sh
+++ sbatch-scripts/o_simple-train/run.sh
@@ -10,15 +10,15 @@
 #SBATCH --mem=64G
 #SBATCH --time=08:00:00
 
+#SBATCH --output=/home/hf201627/dev/lab2/vqa_regat/sbatch-logs/o_simple-train
 module load CUDA/11.8.0
-source /rwthfs/rz/cluster/home/hf201627/miniconda3/bin/activate
-conda activate regat-origin
+source /rwthfs/rz/cluster/home/hf201627/miniconda3/bin/activate regat-origin
 
 # By default, run.sh operates in code directory
 #cd ${REGAT_CODE_PATH}
 
 # Program Section
-python3 main.py --config config/butd_vqa-o.json --epochs 1
+python main.py --config config/butd_vqa-o.json --epochs 1
 # python3 eval.py --output_folder pretrained_models/regat_implicit/ban_1_implicit_vqa_196
 
 
