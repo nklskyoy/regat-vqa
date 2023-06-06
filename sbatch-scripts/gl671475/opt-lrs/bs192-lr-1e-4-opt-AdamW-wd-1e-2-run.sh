add48a986efa372cfae588fd092a69ae08e0c9c7
AtanasGruev <atanas.gruev@rwth-aachen.de>
2023-06-06 21:25:17 +0200

    adding some AdamW-wd-1e-2 scripts

---
+++ sbatch-scripts/gl671475/opt-lrs/bs192-lr-1e-4-opt-AdamW-wd-1e-2-run.sh
@@ -0,0 +1,49 @@
+#!/usr/local_rwth/bin/zsh 
+
+### SBATCH Section
+
+#SBATCH --job-name=VQA-ReGAT
+#SBATCH --output=%j-bs192-lr-1e-4-optim-AdamW-wd-1e-2-output.log
+#SBATCH --error=%j-bs192-lr-1e-4-optim-AdamW-wd-1e-2-error.log
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:volta:2
+#SBATCH --mem-per-gpu=32G
+#SBATCH --time=08:00:00
+#SBATCH --account=lect0099
+
+module load CUDA/11.8.0
+source /rwthfs/rz/cluster/home/gl671475/miniconda3/bin/activate
+conda activate regat_common
+
+export WANDB_API_KEY=32abfb91875c02ffd9d1f8d294baad4f5eabbdf7
+
+# Extracts the TID of the user
+USERNAME=$(whoami)
+
+REGAT_LOCAL_CODE_PATH="/rwthfs/rz/cluster/home/${USERNAME}/vqa_regat"
+REGAT_DATA_AND_MODELS_PATH="/rwthfs/rz/cluster/hpcwork/lect0099" 
+REGAT_SAVE_MODELS_TRAIN_PATH="${REGAT_DATA_AND_MODELS_PATH}/saved_models/${USERNAME}"
+
+# By default, run.sh operates in code directory
+cd ${REGAT_LOCAL_CODE_PATH}
+
+### Program Section
+python3 main.py \
+    --config config/ban_vqa.json \
+    --relation_type spatial \
+    --epochs 20 \
+    --batch_size 192 \
+    --optimizer "AdamW" \
+    --weight_decay 0.01 \
+    --lr_scheduler "default" \
+    --base_lr 0.0001 \
+    --name "bs_192_lr_1e-4_opt_AdamW_wd_1e-2_ep_20" \
+    --wandb "run" \
+    --job_id ${SLURM_JOB_ID} \
+    --output ${REGAT_SAVE_MODELS_TRAIN_PATH}
+
+REGAT_SAVE_EXPERIMENT_TRAIN_PATH=$(find ${REGAT_SAVE_MODELS_TRAIN_PATH} -type d -name "${SLURM_JOB_ID}*" -print -quit)  
+mv "./sbatch-scripts/opt-lrs/${USERNAME}/${SLURM_JOB_ID}-"*".log" ${REGAT_SAVE_EXPERIMENT_TRAIN_PATH}
+
+# If running this script by itself (i.e. not using the central run.sh to submit jobs), switch to
+# mv "sbatch-scripts/bs-lr/${USERNAME}/${SLURM_JOB_ID}-"*".log" ${REGAT_SAVE_EXPERIMENT_TRAIN_PATH}
