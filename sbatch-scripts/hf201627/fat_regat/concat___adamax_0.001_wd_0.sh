ecf0b2c19c1139ce6472afe5295e0475c60153d0
Oleksandr Nikolskyy <onikolskyy@gmail.com>
2023-07-13 09:55:36 +0200

    add sbatch dir for fatregat

--- sbatch-scripts/hf201627/fat_regat/concat___adamax_0.001_wd_0.sh
+++ sbatch-scripts/hf201627/fat_regat/concat___adamax_0.001_wd_0.sh
@@ -8,10 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:volta:2
 #SBATCH --mem=64G
-#SBATCH --time=12:00:00
-#SBATCH --account=lect0099
+#SBATCH --time=01:00:00
 
 export WANDB_API_KEY=4eeb4bff5fc5678d495fd6ac0a3c0a8bfab3ac3c
+
 
 module load CUDA/11.8.0
 source /home/hf201627/anaconda3/bin/activate regat
@@ -27,14 +27,14 @@ REGAT_SAVE_MODELS_EVAL_PATH="${REGAT_DATA_AND_MODELS_PATH}/saved_models/${USERNA
 
 
 # By default, run.sh operates in code directory
-# cd ${REGAT_LOCAL_CODE_PATH}
+#cd ${REGAT_LOCAL_CODE_PATH}
 
-### Program Section
+# Program Section
 
 # (Uncomment for training)
-python3 fat.py --config config/ban_vqa-o.json  --epochs 25 --name "fat_adamax_0.001_wd_0" --job_id ${SLURM_JOB_ID} --output ${REGAT_SAVE_MODELS_TRAIN_PATH} --wandb run
+python3 main.py --config config/ban_vqa-o.json --relation_type spatial  --lr_scheduler OCLR --epochs 1 --name "adamax_0.001_wd_0" --job_id ${SLURM_JOB_ID} --output ${REGAT_SAVE_MODELS_TRAIN_PATH}
 REGAT_SAVE_EXPERIMENT_TRAIN_PATH=$(find ${REGAT_SAVE_MODELS_TRAIN_PATH} -type d -name "${SLURM_JOB_ID}*" -print -quit)  
-#mv "./sbatch-scripts/${SLURM_JOB_ID}-"*".log" ${REGAT_SAVE_EXPERIMENT_TRAIN_PATH}
+mv "./sbatch-scripts/${SLURM_JOB_ID}-"*".log" ${REGAT_SAVE_EXPERIMENT_TRAIN_PATH}
 
 # (Uncomment for evaluation)
 # python3 eval.py --data_folder ${REGAT_DATA_AND_MODELS_PATH}/data --output_folder ${REGAT_DATA_AND_MODELS_PATH}/pretrained_models/regat_spatial/ban_1_spatial_vqa_1687
