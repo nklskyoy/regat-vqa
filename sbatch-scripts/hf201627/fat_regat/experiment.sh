7597ba667bc385b075cda3ad9a1853a482458b82
Oleksandr Nikolskyy <onikolskyy@gmail.com>
2023-07-23 21:56:57 +0200

    add script for csv-guided hyperparam search

---
+++ sbatch-scripts/hf201627/fat_regat/experiment.sh
@@ -0,0 +1,49 @@
+#!/usr/local_rwth/bin/zsh 
+
+### SBATCH Section
+#SBATCH --account=lect0099
+#SBATCH --job-name=VQA-ReGAT
+#SBATCH --output=%j-output.log
+#SBATCH --error=%j-error.log
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:volta:2
+#SBATCH --mem=64G
+#SBATCH --time=12:00:00
+
+export WANDB_API_KEY=4eeb4bff5fc5678d495fd6ac0a3c0a8bfab3ac3c
+
+module load CUDA/11.8.0
+source /home/hf201627/anaconda3/bin/activate regat
+
+# Extracts the TID of the user
+USERNAME=$(whoami)
+
+REGAT_LOCAL_CODE_PATH="/rwthfs/rz/cluster/home/${USERNAME}/vqa_regat"
+REGAT_DATA_AND_MODELS_PATH="/rwthfs/rz/cluster/hpcwork/lect0099" 
+
+REGAT_SAVE_MODELS_TRAIN_PATH="${REGAT_DATA_AND_MODELS_PATH}/saved_models/${USERNAME}/train"
+REGAT_SAVE_MODELS_EVAL_PATH="${REGAT_DATA_AND_MODELS_PATH}/saved_models/${USERNAME}/eval"
+
+# By default, run.sh operates in code directory
+# cd ${REGAT_LOCAL_CODE_PATH}
+
+### Program Section
+python3 fat.py \
+    --config config/ban_vqa-o.json  \
+    --lr_scheduler OCLR \
+    --epochs 30 \
+    --name "fat_adamax_0.001_wd_0" 
+    --job_id ${SLURM_JOB_ID} 
+    --output ${REGAT_SAVE_MODELS_TRAIN_PATH} 
+    --wandb run
+    --init_lr $1 \
+    --peak_lr $2 \
+    --final_lr $3 \
+    --begin_constant $4 \
+    --begin_decay $5 \
+    --lr_scheduler "custom" \
+    --name "fat_concat?opt=adamax;init_lr=$1;peak_lr=$2;final_lr=$3;begin_constant=$4;begin_decay=$5" \
+
+
+
+
---
+++ sbatch-scripts/hf201627/fat_regat/run.sh
@@ -0,0 +1,13 @@
+#!/usr/local_rwth/bin/zsh 
+
+BASE_DIR="/rwthfs/rz/cluster/home/hf201627/dev/lab2/vqa_regat/sbatch-scripts/hf201627/fat_regat"
+
+tail -n +2 "${BASE_DIR}/hyperparam.csv" | while IFS=";" read -r init_lr peak_lr final_lr begin_constant begin_decay
+do
+        sbatch ${BASE_DIR}/experiment.sh \
+            "$init_lr"\
+            "$peak_lr"\
+            "$final_lr"\
+            "$begin_constant"\
+            "$begin_decay"
+done
